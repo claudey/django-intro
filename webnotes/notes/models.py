b8f5efa574c822779ead6a99fb3c7ee19e6879cf
claudey <clauditey@gmail.com>
2012-07-03 16:07:37 +0000

    Hello Heroku World. We gon' have some fun.

--- webnotes/notes/models.py
+++ webnotes/notes/models.py
@@ -4,5 +4,7 @@ from django.db import models
 class Notes(models.Model):
 	title = models.CharField(max_length=255)
 	content = models.TextField()
+	author = models.CharField(max_length=255)
+		
 	def __unicode__(self):
 		return self.title
