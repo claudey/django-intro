18d1e22fdfd2958fdbfb25b5b39e66ec6d8a7fb2
claudey <clauditey@gmail.com>
2012-07-03 12:15:40 +0000

    I didn't know whether commiting this was necessary but I had to do it anyway. Was fun with the first Django lab. hope the others stay the same

---
+++ webnotes/notes/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class Notes(models.Model):
+	title = models.CharField(max_length=255)
+	content = models.TextField()
+	def __unicode__(self):
+		return self.title
